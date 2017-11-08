aeff4912773b1b8229559da4721654826611a788
mandlerm <amaya.melissa@gmail.com>
2017-11-08 17:22:10 -0600

    student login page successful and routes to show page

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -9,8 +9,23 @@ class StudentsController <  ApplicationController
   end
 
   post '/students/new' do
-    binding.pry
+    student = Student.create(params[:student])
+    session[:id] = student.id
+    # if params[:instruments]
+    #   student.instruments << params[:instrument]
+    # end
+    #
+    # if params[:subjects]
+    #   student.instruments << params[:subjects]
+    # end
+
     redirect '/students/show'
+  end
+
+  get '/students/show' do
+    @student = Student.find(session[:id])
+
+    erb :'/students/show'
   end
 
   get '/students/login' do
--- app/models/students.rb
+++ app/models/students.rb
@@ -1,4 +1,6 @@
 class Student < ActiveRecord::Base
+    has_secure_password
+
   has_many :instruments
   has_many :subjects
   belongs_to :teacher
