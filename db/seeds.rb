c200c0fe46929e2540a10031d3d043b8e2282c83
mandlerm <amaya.melissa@gmail.com>
2017-11-09 11:22:10 -0600

    create table view for teacher edit page

--- db/seeds.rb
+++ db/seeds.rb
@@ -9,3 +9,10 @@ Instrument.create(name: 'Piano')
 Instrument.create(name: 'Violin')
 Instrument.create(name: 'Guitar')
 Instrument.create(name: 'Harmonics')
+
+Student.create(name: 'Abigail', grade: 6, password: 'abigail')
+Student.create(name: 'Annabella', grade: 5, password: 'annabella')
+Student.create(name: 'Alexander', grade: 3, password: 'alex')
+Student.create(name: 'Austin', grade: 1, password: 'austin')
+
+Teacher.create(name: 'Melissa', password: 'melissa')
