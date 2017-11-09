b8e57f401b686d0914280e19ef2030ad412afc23
mandlerm <amaya.melissa@gmail.com>
2017-11-09 08:16:39 -0600

    additions to teachers view

--- app/controllers/teachers_controller.rb
+++ app/controllers/teachers_controller.rb
@@ -8,4 +8,29 @@ class TeachersController <  ApplicationController
     erb :'/teachers/login'
   end
 
+  post '/teachers/login' do
+    @teacher = Teacher.find_by(name: params[:name])
+    if @teacher && @teacher.authenticate(params[:password])
+      session[:id] = @teacher.id
+      redirect '/teachers/show'
+    else
+      redirect '/teachers/login'
+    end
+  end
+
+  post '/teachers/new' do
+    teacher = Teacher.create(params[:teacher])
+    session[:id] = teacher.id
+    redirect '/teachers/show'
+  end
+
+  get '/teachers/show' do
+    @teacher = Teacher.find(session[:id])
+    erb :'/teachers/show'
+  end
+
+  get '/teachers/:id/edit' do
+    @teacher = Teacher.find(session[:id])
+    redirect :'/teachers/edit_teacher'
+  end
 end
