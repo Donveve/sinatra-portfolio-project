95082f49ca81e69eb724463dcbf3ea0387035595
mandlerm <amaya.melissa@gmail.com>
2017-11-27 09:18:29 -0600

    comment out all sqlite3 references

--- config/environment.rb
+++ config/environment.rb
@@ -7,7 +7,7 @@ Bundler.require(:default, ENV['SINATRA_ENV'])
 
 configure :development do
   ActiveRecord::Base.establish_connection(
-  :adapter => "sqlite3",
+#  :adapter => "sqlite3",
   :database => "db/#{ENV['SINATRA_ENV']}.sqlite"
 )
 end
